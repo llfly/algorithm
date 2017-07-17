a4fab44b307c12aab7898be9dc5604c8c6fa8d37
llfly <lflylfy@gmail.com>
2017-07-17 16:36:03 +0800

    add create

---
+++ leetcode/108-Convert-Sorted-Array-To-Binary-Search-Tree.js
@@ -0,0 +1,19 @@
+/*
+Given an array where elements are sorted in ascending order, convert it to a height balanced BST.
+*/
+
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {number[]} nums
+ * @return {TreeNode}
+ */
+var sortedArrayToBST = function(nums) {
+      
+};
---
+++ test2/create.js
@@ -0,0 +1,32 @@
+const fs = require('fs');
+const path = require('path');
+
+const textReg = /\.js$/;
+
+
+const compose = (first, ...last) => (...initArgs) =>
+    last.reduce((compose, func) =>
+        func(compose), first(...initArgs))
+
+
+// turn hello world ==> Hello-World
+const camelCase = (str) =>
+    str.replace(/\./, '').split(' ').map(item =>
+        `${String(item).charAt(0).toUpperCase()}${String(item).substr(1)}`).join('-')
+
+// fix postfix ==> Hello-World.js
+const postfix = (filename) => !textReg.test(filename) ?
+    filename += `.js` : filename
+
+
+const filePath = (filename) => (basePath) => path.join(__dirname, basePath, filename)
+
+// createFile
+const createFile = (filepath) => {
+    fs.writeFile(filepath, '/**/', function (err) {
+        if (err) throw err;
+        console.log("create File Success!");
+    });
+}
+
+createFile(compose(camelCase, postfix, filePath)('122222')('../leetcode'));
